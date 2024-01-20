11b75e3907ebdeedc424e7cb2ded7b43a66cd0c8
Ryan Hou <rhou@utexas.edu>
2024-01-20 20:30:00 +0000

    I have passed test 7. Now in test 11

--- wish.c
+++ wish.c
@@ -1,135 +1,381 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <fcntl.h>
-#include <sys/stat.h>
+#include "wish.h"
 
+const char * built_in_commands[] = {
+    "exit",
+    "cd",
+    "path"
+};
 
-#define MAX_LINE 80 //MAX LINE
+void param_check (const char * fname, int line_no, int n_args, ...)
+{
+    va_list var_list;
+    va_start(var_list, n_args);
 
-int main(void) {
-    char **args = malloc((MAX_LINE/2 + 1) * sizeof(char*)); // COMMAND LINE ARGUMENTS
-    int should_run = 1; // WHEN THE PROGRAM SHOULD EXIT
-    char **path = malloc(2 * sizeof(char*)); // INITIAL SHELL PATH AS SPECIFIED
-    path[0] = "/bin";
-    path[1] = NULL;
+    for (int i = 0; i < n_args; i++)
+    {
+        char * param = va_arg(var_list, char *);
+        CHECK((i + 1), fname, line_no, param);
+    }
 
-    while (should_run) {
-        printf("wish> ");
-        fflush(stdout);
+    va_end(var_list);
+}
 
-        // READ USER INPUT
-        char *line = NULL;
-        size_t len = 0;
-        ssize_t nread = getline(&line, &len, stdin);  //USE GETLINE AS SPECIFIED
+static void close_file(FILE * p_file)
+{
+    if (NULL != p_file)
+    {
+        fclose(p_file);
+    }
+}
 
-        // CHECK FOR EOF
-        if (nread == -1) {
-            exit(0);
+static void handle_error(FILE * p_file)
+{
+    char error_message[30] = "An error has occurred\n"; 
+    write(STDERR_FILENO, error_message, strlen(error_message));
+    close_file(p_file);
+}
+
+static int compare(const char * p_str1, const char * p_str2)
+{
+    param_check(__FILE__, __LINE__, ARG_2, p_str1, p_str2);
+
+    while(('\0' != *p_str1) &&
+    ('\0' != *p_str2))
+    {
+        if (*p_str1 != *p_str2)
+        {
+            return -1;
         }
 
-        // REMOVE ANY TRAILING CHARACTER
-        line[strcspn(line, "\n")] = 0;
+        p_str1++;
+        p_str2++;
+    }
 
-        // TOKENIZE
-        char *token;
-        token = strsep(&line, " ");
+    if (('\0' == *p_str1) &&
+    ('\0' == *p_str2))
+    {
+        return 0;
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+int is_built_in (const char * p_command)
+{
+    if (NULL == p_command)
+    {
+        return 0;
+    }
+
+    size_t cmd_sz = sizeof(built_in_commands) / sizeof(built_in_commands[0]);
+    for (int i = 0; i < cmd_sz; i++)
+    {
+        if (0 == compare(p_command, built_in_commands[i]))
+        {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+static char * copy (const char * p_data, size_t length)
+{
+    param_check(__FILE__, __LINE__, ARG_1, p_data);
+
+    char * p_new = calloc((length + 1), sizeof(char));
+    if (NULL == p_new)
+    {
+        handle_error(NULL);
+        exit(1);
+    }
+
+    memcpy(p_new, p_data, length);
+
+    return p_new;
+}
+
+static size_t getlen (const char * p_str)
+{
+    const char * p_iter = NULL;
+    for (p_iter = p_str; *p_iter; ++p_iter)
+    {
+        continue;
+    }
+
+    return (p_iter - p_str);
+}
+
+void execute_builtins (const char * p_command, char ** pp_args, char ** pp_paths)
+{
+    param_check(__FILE__, __LINE__, ARG_3, p_command, pp_args, pp_paths);
+
+    if (0 == compare(p_command, built_in_commands[0]))
+    {
+        if (NULL != pp_args[1])
+        {
+            handle_error(NULL);
+        }
+
+        for (int i = 0; (i < MAX_ARGS) && (NULL != pp_args[i]); i++)
+        {
+            CLEAR(pp_args[i]);
+        }
+    }
+    else if (0 == compare(p_command, built_in_commands[1]))
+    {
+        if (NULL != pp_args[1])
+        {
+            if (0 != chdir(pp_args[1]))
+            {
+                handle_error(NULL);
+            }
+        }
+        else
+        {
+            handle_error(NULL);
+        }
+    }
+    else if (0 == compare(p_command, built_in_commands[2]))
+    {
+        for (int i = 0; NULL != pp_paths[i]; i++)
+        {
+            CLEAR(pp_paths[i]);
+        }
+
         int i = 0;
-        while (token != NULL) {
-            args[i] = token;
+        while (NULL != pp_args[i + 1])
+        {
+            size_t arg_sz = getlen(pp_args[i + 1]);
+            pp_paths[i] = copy(pp_args[i + 1], arg_sz);
             i++;
-            token = strsep(&line, " ");
         }
-        args[i] = NULL;
+        pp_paths[i] = NULL;
+    }
+}
 
-        // OUTPUT REDIRECTION CHECK
-        char *outputFile = NULL;
-        for (i = 0; args[i] != NULL; i++) {
-            if (strcmp(args[i], ">") == 0) {
-                args[i] = NULL;
-                outputFile = args[i+1];
+void parse_input (char * p_input, char ** pp_args)
+{
+    param_check(__FILE__, __LINE__, ARG_2, p_input, pp_args);
+
+    char * p_token  = strtok(p_input, " \t\n");
+    int i = 0;
+    while ((NULL != p_token) &&
+        ((MAX_ARGS - 1) > i))
+    {
+        size_t token_sz = getlen(p_token);
+        pp_args[i] = copy(p_token, token_sz);
+        i++;
+        p_token = strtok(NULL, " \t\n");
+    }
+
+    pp_args[i] = NULL;
+    CLEAR(p_token);
+}
+
+void process_command (char * p_input, char ** pp_paths)
+{
+    param_check(__FILE__, __LINE__, ARG_2, p_input, pp_paths);
+
+    char ** pp_args = calloc(MAX_ARGS, sizeof(char *));
+    if (NULL == pp_args)
+    {
+        handle_error(NULL);
+        exit(1);
+    }
+
+    parse_input(p_input, pp_args);
+
+    int redirect_output = 0;
+    char * p_out_file = NULL;
+
+    for (int i = 0; NULL != pp_args[i]; i++)
+    {
+        if (0 == compare(pp_args[i], ">"))
+        {
+            if ((NULL != pp_args[i + 1]) &&
+                (NULL == pp_args[i + 2]))
+            {
+                size_t filename_len = getlen(pp_args[i + 1]);
+                p_out_file = copy(pp_args[i + 1], filename_len);
+                redirect_output = 1;
+                pp_args[i] = NULL;
+                break;
+            }
+            else if (NULL != pp_args[i + 2])
+            {
+                handle_error(NULL);
+                for (int j = 0; j < i; j++)
+                {
+                    CLEAR(pp_args[j]);
+                }
+
+                CLEAR(pp_args);
+                return;
+            }
+            else
+            {
+                handle_error(NULL);
+                for (int j = 0; j < i; j++)
+                {
+                    CLEAR(pp_args[j]);
+                }
+
+                CLEAR(pp_args);
+                return;
+            }
+        }
+    }
+
+    if (is_built_in(pp_args[0]))
+    {
+        execute_builtins(pp_args[0], pp_args, pp_paths);
+    }
+    else
+    {
+        char cmd_path[MAX_INPUT]    = { 0 };
+        int cmd_found               = 0;
+
+        for (int i = 0; NULL != pp_paths[i]; i++)
+        {
+            snprintf(cmd_path, sizeof(cmd_path), "%s/%s", pp_paths[i], pp_args[0]);
+
+            if (0 == access(cmd_path, X_OK))
+            {
+                cmd_found = 1;
                 break;
             }
         }
 
-        // BUILT IN COMMANDS
-        if (strcmp(args[0], "exit") == 0) { //EXIT COMMAND
-            if (args[1] != NULL) {
-                fprintf(stderr, "Error: exit does not take any arguments.\n");
-                continue;
-            }
-            should_run = 0; //IF EXIT IS ENTD, CLOSE THE SHELL
-        } else if (strcmp(args[0], "cd") == 0) { //CD COMMAND
-            if (args[1] == NULL || args[2] != NULL) {
-                fprintf(stderr, "Error: cd takes exactly one argument.\n");
-                continue;
-            }
-            if (chdir(args[1]) != 0) {
-                perror("Error");
-                continue;
-            }
-        } else if (strcmp(args[0], "path") == 0) {
-            // FREE EXISTING PATH   5 PATH
-            free(path);
-            path = malloc((i+1) * sizeof(char*));
-            // UPDATE THE PATH WITH THE NEW DIRECTORIES
-            for (i = 0; args[i+1] != NULL; i++) {
-                path[i] = args[i+1];
-            }
-            path[i] = NULL;
-        } else {
-            // CHECK IF THE COMMAND EXISTS & IT IS EXECUTABLE
-            char cmd[MAX_LINE];
-            int found = 0;
-            for (i = 0; path[i] != NULL; i++) {
-                snprintf(cmd, sizeof(cmd), "%s/%s", path[i], args[0]);
-                if (access(cmd, X_OK) == 0) {
-                    found = 1;
-                    break;
-                }
-            }
-
-            if (!found) {  //IF COMMAND IS NOT FIND PRINT ERROR
-                fprintf(stderr, "Command not found.\n");
-                continue;
-            }
-
-            // IF THE COMMAND IS FOUND, THEN FORK A CHILD PROCESS
+        if (cmd_found)
+        {
             pid_t pid = fork();
-            if (pid < 0) {
-                fprintf(stderr, "Fork failed.\n");
-                return 1;
-            } else if (pid == 0) {
-                // REDIRECT OUTPUT WHERE NECESSARY
-                if (outputFile != NULL) {
-                    int fd = open(outputFile, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-                    if (fd == -1) {
-                        perror("Error opening file for writing");
-                        return 1;
+            if (0 == pid)
+            {
+                if (redirect_output)
+                {
+                    int fd = open(p_out_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+                    if (-1 == fd)
+                    {
+                        handle_error(NULL);
+                        exit(1);
                     }
                     dup2(fd, STDOUT_FILENO);
                     dup2(fd, STDERR_FILENO);
                     close(fd);
                 }
 
-                // EXECUTE THE COMMAND OF THE CHILD PROCESS
-                if (execv(cmd, args) < 0) {
-                    fprintf(stderr, "Execution failed.\n");
-                    return 1;
+                if (-1 == execv(cmd_path, pp_args))
+                {
+                    handle_error(NULL);
                 }
-            } else {
-                // PARENT PROCESS, WAIT FOR CHILD PROCESS TO COMPLETE
-                wait(NULL);
+            }
+            else if (0 > pid)
+            {
+                handle_error(NULL);
+            }
+            else
+            {
+                int status = 0;
+                wait(&status);
             }
         }
+        else
+        {
+            handle_error(NULL);
+        }
 
-        free(line);
+        if (redirect_output)
+        {
+            if (NULL != p_out_file)
+            {
+                CLEAR(p_out_file);
+            }
+        }
     }
 
-    free(args);
-    free(path);
+    for (int i = 0; (i < MAX_ARGS) && (NULL != pp_args[i]); i++)
+    {
+        CLEAR(pp_args[i]);
+    }
+    CLEAR(pp_args);
+}
+
+int main (int argc, char * argv[])
+{
+    if (MAX_EX_ARGS < argc)
+    {
+        handle_error(NULL);
+        exit(1);
+    }
+
+    char    * p_input           = 0;
+    size_t    input_sz          = 0;
+    char    * pp_dirs[MAX_ARGS] = { 0 };
+    
+    size_t dirlen   = getlen("/bin");
+    pp_dirs[0]      = copy("/bin", dirlen);
+    pp_dirs[1]      = NULL;
+
+    FILE * p_batch = NULL;
+    if (2 == argc)
+    {
+        p_batch = fopen(argv[1], "r");
+        if (NULL == p_batch)
+        {
+            handle_error(NULL);
+            exit(1);
+        }
+    }
+    
+    while(1)
+    {
+        if ((1 == argc) &&
+            (0 == compare(argv[0], "./wish")))
+        {
+            printf("\nwish> ");
+            ssize_t bytes_read = getline(&p_input, &input_sz, stdin);
+            if ((-1 == bytes_read) ||
+                (feof(stdin)))
+            {
+                break;
+            }
+        }
+        else if ((2 == argc) &&
+                (0 == compare(argv[0], "./wish")))
+        {
+            ssize_t bytes_read = getline(&p_input, &input_sz, p_batch);
+            if ((-1 == bytes_read) ||
+                (feof(p_batch)))
+            {
+                break;
+            }
+        }
+        else
+        {
+            handle_error(NULL);
+        }
+
+        process_command(p_input, pp_dirs);
+
+        if (0 == compare(p_input, built_in_commands[0]))
+        {
+            break;
+        }
+
+        CLEAR(p_input);
+    }
+
+    close_file(p_batch);
+
+    CLEAR(p_input);
+    
+    for (int i = 0; NULL != pp_dirs[i]; i++)
+    {
+        CLEAR(pp_dirs[i]);
+    }
 
     return 0;
 }
